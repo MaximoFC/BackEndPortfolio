aee66a39cc415bf78a3c62279deebfbfa77c9957
MaximoFC <maximofcallejas@gmail.com>
2023-01-31 20:45:39 -0300

    Cambio en el repository de Persona

--- src/main/java/com/portfolio2/mfc2/Repository/IPersonaRepository.java
+++ src/main/java/com/portfolio2/mfc2/Repository/IPersonaRepository.java
@@ -1,10 +1,12 @@
 package com.portfolio2.mfc2.Repository;
 
 import com.portfolio2.mfc2.Entity.Persona;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IPersonaRepository extends JpaRepository<Persona, Long>{
-    
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
 }
--- src/main/java/com/portfolio2/mfc2/Repository/Rhys.java
+++ src/main/java/com/portfolio2/mfc2/Repository/Rhys.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface Rhys extends JpaRepository<hys, Integer>{
-    Optional<hys> findByNombre(String nombre);
+    public Optional<hys> findByNombre(String nombre);
     public boolean existsByNombre(String nombre);
 }
